7d5e33abdc8e9a75bf443f9ecc701cfe97950ac7
Tushar <Tushar@Mac.lan>
2025-06-17 22:38:38 +0530

    onboarding added

---
+++ screens/Onboarding/onboard11.dart
@@ -0,0 +1,242 @@
+import 'package:figma_to_flutter/gen/assets.gen.dart';
+import 'package:flutter/material.dart';
+
+class Onboarding11Screen extends StatefulWidget {
+  const Onboarding11Screen({Key? key}) : super(key: key);
+
+  @override
+  State<Onboarding11Screen> createState() => _OnboardingScreenState();
+}
+
+class _OnboardingScreenState extends State<Onboarding11Screen> {
+  late PageController _controller;
+
+  @override
+  void initState() {
+    _controller = PageController();
+    super.initState();
+  }
+
+  int _currentPage = 0;
+  List colors = const [
+    Color(0xffDAD3C8),
+    Color(0xffFFE5DE),
+    Color(0xffDCF6E6),
+  ];
+
+  AnimatedContainer _buildDots({
+    int? index,
+  }) {
+    return AnimatedContainer(
+      duration: const Duration(milliseconds: 200),
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.all(
+          Radius.circular(50),
+        ),
+        color: Color(0xFF000000),
+      ),
+      margin: const EdgeInsets.only(right: 5),
+      height: 10,
+      curve: Curves.easeIn,
+      width: _currentPage == index ? 20 : 10,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    double width = SizeConfig.screenW!;
+    double height = SizeConfig.screenH!;
+
+    return Scaffold(
+      backgroundColor: colors[_currentPage],
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: PageView.builder(
+                physics: const BouncingScrollPhysics(),
+                controller: _controller,
+                onPageChanged: (value) => setState(() => _currentPage = value),
+                itemCount: contents.length,
+                itemBuilder: (context, i) {
+                  return Padding(
+                    padding: const EdgeInsets.all(40.0),
+                    child: Column(
+                      children: [
+                        Expanded(
+                          // Wrap the Image.asset with Expanded
+                          child: Image.asset(
+                            contents[i].image,
+                            // Remove fixed height here, let it be flexible
+                          ),
+                        ),
+                        SizedBox(
+                          height: (height >= 840) ? 60 : 30,
+                        ),
+                        Text(
+                          contents[i].title,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontFamily: "Mulish",
+                            fontWeight: FontWeight.w600,
+                            fontSize: (width <= 550) ? 30 : 35,
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        Text(
+                          contents[i].desc,
+                          style: TextStyle(
+                            fontFamily: "Mulish",
+                            fontWeight: FontWeight.w300,
+                            fontSize: (width <= 550) ? 17 : 25,
+                          ),
+                          textAlign: TextAlign.center,
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(
+                      contents.length,
+                      (int index) => _buildDots(
+                        index: index,
+                      ),
+                    ),
+                  ),
+                  _currentPage + 1 == contents.length
+                      ? Padding(
+                          padding: const EdgeInsets.all(30),
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text("START"),
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.black,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(50),
+                              ),
+                              padding: (width <= 550)
+                                  ? const EdgeInsets.symmetric(
+                                      horizontal: 100, vertical: 20)
+                                  : EdgeInsets.symmetric(
+                                      horizontal: width * 0.2, vertical: 25),
+                              textStyle:
+                                  TextStyle(fontSize: (width <= 550) ? 13 : 17),
+                            ),
+                          ),
+                        )
+                      : Padding(
+                          padding: const EdgeInsets.all(30),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              TextButton(
+                                onPressed: () {
+                                  _controller.jumpToPage(2);
+                                },
+                                child: const Text(
+                                  "SKIP",
+                                  style: TextStyle(color: Colors.black),
+                                ),
+                                style: TextButton.styleFrom(
+                                  elevation: 0,
+                                  textStyle: TextStyle(
+                                    fontWeight: FontWeight.w600,
+                                    fontSize: (width <= 550) ? 13 : 17,
+                                  ),
+                                ),
+                              ),
+                              ElevatedButton(
+                                onPressed: () {
+                                  _controller.nextPage(
+                                    duration: const Duration(milliseconds: 200),
+                                    curve: Curves.easeIn,
+                                  );
+                                },
+                                child: Text("NEXT",
+                                    style: TextStyle(color: Colors.white)),
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: Colors.black,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(50),
+                                  ),
+                                  elevation: 0,
+                                  padding: (width <= 550)
+                                      ? const EdgeInsets.symmetric(
+                                          horizontal: 30, vertical: 20)
+                                      : const EdgeInsets.symmetric(
+                                          horizontal: 30, vertical: 25),
+                                  textStyle: TextStyle(
+                                      fontSize: (width <= 550) ? 13 : 17),
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class OnboardingContents {
+  final String title;
+  final String image;
+  final String desc;
+
+  OnboardingContents({
+    required this.title,
+    required this.image,
+    required this.desc,
+  });
+}
+
+List<OnboardingContents> contents = [
+  OnboardingContents(
+    title: "Track Your work and get the result",
+    image: Assets.images.image1.path,
+    desc: "Remember to keep track of your professional accomplishments.",
+  ),
+  OnboardingContents(
+    title: "Stay organized with team",
+    image: Assets.images.image2.path,
+    desc:
+        "But understanding the contributions our colleagues make to our teams and companies.",
+  ),
+  OnboardingContents(
+    title: "Get notified when work happens",
+    image: Assets.images.image3.path,
+    desc:
+        "Take control of notifications, collaborate live or on your own time.",
+  ),
+];
+
+class SizeConfig {
+  static MediaQueryData? _mediaQueryData;
+  static double? screenW;
+  static double? screenH;
+  static double? blockH;
+  static double? blockV;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenW = _mediaQueryData!.size.width;
+    screenH = _mediaQueryData!.size.height;
+    blockH = screenW! / 100;
+    blockV = screenH! / 100;
+  }
+}
